5ba5d76978fe79be5fc12a44662b1fdf0d288573
Lewis Watson <lewiswatson55@protonmail.com>
2021-04-10 00:31:08 +0100

    Basic Menu

--- main.c
+++ main.c
@@ -1,6 +1,43 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+char menuSelection;
+
+void startGame();
+void otherOption();
+
+void menu(){
+
+    printf("\nWelcome to Connect Four - CLI Edition!\n\nPlease select from the menu then press enter...\n\n1. Start Game\n2. Other Option\n0. Exit\nChoice:  ");
+    fgets(&menuSelection,8,stdin);
+
+    switch (menuSelection) {
+        case '0':
+            return;
+            break;
+        case '1':
+            startGame();
+            break;
+        case '2':
+            otherOption();
+            break;
+
+        default:
+            printf("Invalid Choice... %c", menuSelection);
+            break;
+    }
+}
+
+
+int main(int argc, char **argv)
+{
+    menu();
     return 0;
+}
+
+void startGame(){
+    printf("\nStart Game\n");
+}
+
+void otherOption(){
+    printf("\n1Other Option\n");
 }
