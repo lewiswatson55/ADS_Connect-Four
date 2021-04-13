63bef96fc01e8519fdc8cbf7e2db75cca30a3fc5
Lewis Watson <lewiswatson55@protonmail.com>
2021-04-13 05:14:21 +0100

    Added game type variable

--- games.h
+++ games.h
@@ -8,6 +8,7 @@
 typedef struct {
     char *name1, *name2;
     int rowSize, columnSize;
+    int gameType;
     struct Position* board;
 } Game;
 
@@ -19,5 +20,9 @@ struct Position {
 struct Position* createBoard(int column, int row, int numberOfColumns, int numberOfRows);
 void newGame(char *player1, char *player2, int columns, int rows);
 void displayBoard(struct Position* board, int width);
+void gameController();
+void player1Move(),player2Move();
+void checkDown(),checkRight(),checkDiagonal();
+void gravityMove(); //Animate dropping?
 
 #endif //ADSCOURSEWORK_GAMEBOARD_H
