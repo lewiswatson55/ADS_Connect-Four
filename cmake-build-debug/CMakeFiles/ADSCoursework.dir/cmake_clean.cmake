aa4fc1071c8462a47ebba643b4499094c33778f8
Lewis Watson <lewiswatson55@protonmail.com>
2021-04-20 23:55:28 +0100

    Unversioned Files

---
+++ cmake-build-debug/CMakeFiles/ADSCoursework.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "ADSCoursework.exe"
+  "ADSCoursework.exe.manifest"
+  "ADSCoursework.lib"
+  "ADSCoursework.pdb"
+  "CMakeFiles/ADSCoursework.dir/games.c.obj"
+  "CMakeFiles/ADSCoursework.dir/main.c.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ADSCoursework.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
